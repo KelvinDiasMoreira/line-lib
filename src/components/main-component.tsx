2836bca7e3a90c42f629d26cb8182ed14c80dfc3
Kelvin <kelvingremista2015@hotmail.com>
2025-10-21 22:43:07 -0300

    ref: best way

--- src/components/main-component.tsx
+++ src/components/main-component.tsx
@@ -1,10 +1,10 @@
-import React, { ReactNode, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 
 interface MainComponentProps {
   children: React.ReactNode;
 }
 
-interface StyleProps extends React.CSSProperties {}
+interface StyleProps extends React.CSSProperties { }
 
 interface IProps {
   style: StyleProps;
@@ -19,250 +19,37 @@ interface ChildrenProps extends React.CSSProperties {
 }
 
 export function MainComponent({ children }: MainComponentProps) {
-  const [boundRect, setBoundRect] = useState<DOMRect>();
+  const firstElement = React.Children.toArray(children)[0] as ChildrenProps
 
-  const firstChild = React.Children.toArray(children)[0] as ChildrenProps;
-  let childInsideIsArr = true;
-  const childRef = useRef<HTMLElement>(null);
+  if (firstElement.type !== 'div') {
+    return <p>first element must be a div</p>
+  }
+  const firstElementRef = useRef<HTMLElement>(null)
+  const childrensRefs = useRef<HTMLElement[]>([])
+  const { props: { children: childrens } } = firstElement
+  const clonedChildrens = childrens.map((child, idx) => {
+    return React.cloneElement(child, {
+      //@ts-ignore
+      ref: (el: HTMLElement) => {
+        childrensRefs.current[idx] = el
+      },
+      key: idx
+    })
+  })
 
   useEffect(() => {
-    if (childRef.current) {
-      const rect = childRef.current.getBoundingClientRect();
-      setBoundRect(rect);
+    if (firstElementRef.current) {
+      /* TODO */
     }
-  }, []);
-
-  const { type, props } = firstChild;
-  const { style, children: childrenInside } = props;
-
-  if (type !== "div") {
-    return "must be a div";
-  }
-
-  if (!childrenInside) return children;
-
-  const lines: React.JSX.Element[] = [];
-
-  /**
-   * TODO: ruler
-   */
-  const widthChild = style?.width ?? 0;
-  const heightChild = style?.height ?? 0;
-
-  if (childrenInside) {
-    if (!boundRect) {
-      childInsideIsArr = false;
-    } else {
-      if (!Array.isArray(childrenInside)) {
-        childInsideIsArr = false;
-        buildLinesWithOneChildren(childrenInside, boundRect, lines);
-      } else {
-        /**
-         * TODO: fix when elements has diferent height
-         */
-        buildLinesWithMultipleChildren(childrenInside, boundRect, lines);
-      }
+    if (childrensRefs.current) {
+      /* TODO */
     }
-  }
+  }, [])
 
-  const cloned = React.cloneElement(firstChild, {
+  return React.cloneElement(firstElement, {
     //@ts-ignore
-    children: childInsideIsArr
-      ? [...childrenInside, ...lines]
-      : [childrenInside, ...lines],
-    ref: childRef,
-  });
-
-  return cloned;
-}
-
-function buildLinesWithOneChildren(
-  childrenInside: ChildrenProps,
-  boundRect: DOMRect,
-  linesRef: React.JSX.Element[]
-) {
-  const child = childrenInside;
-  const { style } = child.props;
-  const { width, height, left, top, right, bottom } = style;
-  if (!width || !height || !left || !top) return;
-  if (
-    typeof top === "string" ||
-    typeof height === "string" ||
-    typeof width === "string" ||
-    typeof left === "string"
-  ) {
-    return;
-  }
-  let i = 0;
-
-  const middle = boundRect.y + top + height * 0 + height / 2;
-
-  const maxWidthLineLeft = width + left - width * (0 === 0 ? 1 : i);
-  const maxHeightTopSide = top + (i === 0 ? 0 : height) * (i === 0 ? 1 : i);
-  const maxWidthLineRight = boundRect.width - maxWidthLineLeft - width;
-  const maxHeightBottomSide = boundRect.bottom - (middle + height / 2);
-
-  /**
-   * Left side
-   */
-  linesRef.push(
-    <div
-      style={{
-        position: "absolute",
-        width: maxWidthLineLeft,
-        height: 1,
-        backgroundColor: "red",
-        left: boundRect.x,
-        top: middle,
-      }}
-      key={`line-left-${i}`}
-    />
-  );
-
-  /**
-   * Top side
-   */
-  linesRef.push(
-    <div
-      style={{
-        position: "absolute",
-        width: 1,
-        height: maxHeightTopSide,
-        backgroundColor: "red",
-        left: boundRect.x + maxWidthLineLeft + width / 2,
-        top: boundRect.y,
-      }}
-      key={`line-top-${i}`}
-    />
-  );
-
-  /**
-   * Right side
-   */
-  linesRef.push(
-    <div
-      style={{
-        position: "absolute",
-        width: maxWidthLineRight,
-        height: 1,
-        backgroundColor: "red",
-        left: boundRect.x + maxWidthLineLeft + width,
-        top: middle,
-      }}
-      key={`line-right`}
-    />
-  );
-
-  /**
-   * Bottom side
-   */
-  linesRef.push(
-    <div
-      style={{
-        position: "absolute",
-        width: 1,
-        height: maxHeightBottomSide,
-        backgroundColor: "red",
-        left: boundRect.x + maxWidthLineLeft + width / 2,
-        top: middle + height / 2,
-      }}
-      key={`line-bottom-`}
-    />
-  );
-}
-
-function buildLinesWithMultipleChildren(
-  childrenInside: ChildrenProps[],
-  boundRect: DOMRect,
-  linesRef: React.JSX.Element[]
-) {
-  for (let i = 0; i < childrenInside.length; i++) {
-    const child = childrenInside[i];
-    const { style } = child.props;
-    const { width, height, left, top, right, bottom } = style;
-    if (!width || !height || !left || !top) continue;
-    if (
-      typeof top === "string" ||
-      typeof height === "string" ||
-      typeof width === "string" ||
-      typeof left === "string"
-    ) {
-      continue;
-    }
-
-    const middle = boundRect.y + top + height * i + height / 2;
-
-    const maxWidthLineLeft = width + left - width * (i === 0 ? 1 : i);
-    const maxHeightTopSide = top + (i === 0 ? 0 : height) * (i === 0 ? 1 : i);
-    const maxWidthLineRight = boundRect.width - maxWidthLineLeft - width;
-    const maxHeightBottomSide = boundRect.bottom - (middle + height / 2);
-
-    /**
-     * Left side
-     */
-    linesRef.push(
-      <div
-        style={{
-          position: "absolute",
-          width: maxWidthLineLeft,
-          height: 1,
-          backgroundColor: "red",
-          left: boundRect.x,
-          top: middle,
-        }}
-        key={`line-left-${i}`}
-      />
-    );
-
-    /**
-     * Top side
-     */
-    linesRef.push(
-      <div
-        style={{
-          position: "absolute",
-          width: 1,
-          height: maxHeightTopSide,
-          backgroundColor: "red",
-          left: boundRect.x + maxWidthLineLeft + width / 2,
-          top: boundRect.y,
-        }}
-        key={`line-top-${i}`}
-      />
-    );
-
-    /**
-     * Right side
-     */
-    linesRef.push(
-      <div
-        style={{
-          position: "absolute",
-          width: maxWidthLineRight,
-          height: 1,
-          backgroundColor: "red",
-          left: boundRect.x + maxWidthLineLeft + width,
-          top: middle,
-        }}
-        key={`line-right-${i}`}
-      />
-    );
-
-    /**
-     * Bottom side
-     */
-    linesRef.push(
-      <div
-        style={{
-          position: "absolute",
-          width: 1,
-          height: maxHeightBottomSide,
-          backgroundColor: "red",
-          left: boundRect.x + maxWidthLineLeft + width / 2,
-          top: middle + height / 2,
-        }}
-        key={`line-bottom-${i}`}
-      />
-    );
-  }
+    ref: firstElementRef,
+    //@ts-ignore
+    children: clonedChildrens
+  })
 }
